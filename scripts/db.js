0a27697c0a1b6c0da8618d34ea0f8d2df8175274
Chrito196 <Christopher-Braun@outlook.com>
2025-12-23 20:06:15 +0100

    optimate overlay and JS.

--- scripts/db.js
+++ scripts/db.js
@@ -1,20 +1,24 @@
 let myDishesBurger = [
     {
+        "image": "./assets/img/dishes/burger/burger0.svg",
         "name": "Veggie mushroom black burger",
         "price": 16.90,
         "description": "Mixeed green salad, Tomatoes, Edamame, Mushrooms",
     },
     {
+        "image": "./assets/img/dishes/burger/burger1.svg",
         "name": "All meat burger",
         "price": 15.90,
         "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
     },
     {
+        "image": "./assets/img/dishes/burger/burger2.svg",
         "name": "Beff red burger",
         "price": 14.90,
         "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
     },
     {
+        "image": "./assets/img/dishes/burger/burger3.svg",
         "name": "Big chicken burger",
         "price": 15.90,
         "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
@@ -23,21 +27,25 @@ let myDishesBurger = [
 
 let myDishesPizza = [
     {
+        "image": "./assets/img/dishes/pizza/pizza0.svg",
         "name": "Pizza Margherita",
         "price": 11.90,
         "description": "Tomato Sauce, Mozzarella",
     },
     {
+        "image": "./assets/img/dishes/pizza/pizza1.svg",
         "name": "Pizza Chorizo", 
         "price": 13.90,
         "description": "Tomato slices, Mozzarella, Chorizo",
     },
     {
+        "image": "./assets/img/dishes/pizza/pizza2.svg",
         "name": "Funghi", 
         "price": 12.90,
         "description": "Red onion, Olives, Button Mushrooms, Mozzarella",
     },
     {
+        "image": "./assets/img/dishes/pizza/pizza3.svg",
         "name": "Quattro Formaggi with Chicken",
         "price": 15.90,
         "description": "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
@@ -46,21 +54,25 @@ let myDishesPizza = [
 
 let myDishesSalad = [
     {
+        "image": "./assets/img/dishes/salad/salad0.svg",
         "name": "Warm beef arugula salad",
         "price": 16.90,
         "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamicvinegar dressing",
     },
     {
+        "image": "./assets/img/dishes/salad/salad1.svg",
         "name": "Mini green Salad",
         "price": 7.90,
         "description": "Green salad, Cucumber, Carrots, Parsley, Radishes",
     },
     {
+        "image": "./assets/img/dishes/salad/salad2.svg",
         "name": "Green Salad with sea food",
         "price": 16.90,
         "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustardlemon dressing with dill",
     },
     {
+        "image": "./assets/img/dishes/salad/salad3.svg",
         "name": "Vegan green salad with tofu",
         "price": 14.90,
         "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
--- scripts/template.js
+++ scripts/template.js
@@ -1,9 +1,19 @@
 function DishCardTemplate(dish) {
   return `
     <div class="dishes-card">
-      <h3>${dish.name}</h3>
-      <p>${dish.description}</p>
-      <div class="price">${dish.price.toFixed(2)} €</div>
+      <div class="dish-img-wrap">
+        <img class="dish-cover" src="${dish.image}" alt="Cover von ${
+    dish.name
+  }">
+      </div>
+
+      <div class="dish-info">
+        <div class="dish-head">
+          <h3 class="dish-name">${dish.name}</h3>
+           <p class="dish-desc">${dish.description}</p>          
+        </div>
+          <div class="dish-price">${dish.price.toFixed(2)} €</div>       
+      </div>
     </div>
   `;
 }
